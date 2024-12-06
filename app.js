6620022b048280f44cec0f9f6b5ec90a4ab67026
Pierre Roitg <pierre.roitg@live.fr>
2024-12-06 14:52:03 +0100

    back maj

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var usersRouter = require("./routes/users");
+
 var app = express();
 const cors = require("cors");
 app.use(cors());
